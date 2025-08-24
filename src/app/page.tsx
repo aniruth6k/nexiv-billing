54814be6597ad7a64610735f28c855fd932e6cfb
aniruth <aniruthprabug@gmail.com>
2025-08-24 22:11:35 +0530

    Change HomePage to async for server-side redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 
-export default function HomePage() {
+export default async function HomePage() {
   // Server-side redirect to hotel route
   // Let /hotel/page.tsx handle the authentication flow
   redirect("/hotel");
-}``
+}
